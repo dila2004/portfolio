516ee3e7b2f9c1f3ea96a64253f3f8b7dc07ada6
Sachintha Dilshan Chandradasa <150923784+dila2004@users.noreply.github.com>
2025-06-25 00:18:32 +0530

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
+  base: '/portfolio/', // Required for GitHub Pages
   plugins: [
-    tailwindcss(),
+    react(),           // Enables React Fast Refresh and JSX
+    tailwindcss(),     // Enables Tailwind
   ],
 })
